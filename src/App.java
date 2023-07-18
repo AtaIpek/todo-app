15823ba29ce4b5a5cb387b3b79fd668a57f823d7
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-07-18 19:29:30 +0300

    Objectives decided

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // // Objective:
+
+        // // ---------- Alper ----------
+        // TodoGenerator todoGenerator = new TodoPrompter();
+        // List<Todo> gottenTodos = todoGenerator.getMultipleTodos();
+
+        // // ---------- Ata ----------
+        // TodoList todoList = new TodoList();
+
+        // for (Todo todo : gottenTodos) {
+        // todoList.add(todo);
+        // }
+        // todoList.print();
+
+        // todoList.sortByPriority();
+        // todoList.print();
     }
 }
